c72a013c6c4008aac5e78457c84afe97dfa68ecd
elepete <peter.wiese@gmx.de>
2021-08-21 14:42:08 +0200

    Update btusb.c
    added an empty line that was missing after the new insertion (for better readability)

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -398,6 +398,7 @@ static const struct usb_device_id blacklist_table[] = {
 						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x0bda, 0x4852), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
+	
 	/* Realtek Bluetooth devices */
 	{ USB_VENDOR_AND_INTERFACE_INFO(0x0bda, 0xe0, 0x01, 0x01),
 	  .driver_info = BTUSB_REALTEK },
